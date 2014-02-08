da467e6a71fdb6ff001481a1d501f91eef06992e
blahshaw <benjaminhlambert@gmail.com>
2014-02-07 17:21:26 -0800

    Added solution for week2 assignment1

--- Week1/Frequent Words with Mismatches/mismatch.py
+++ Week1/Frequent Words with Mismatches/mismatch.py
@@ -1,39 +1,43 @@
 import operator
 
 def find_mismatch(genome, window, mismatch):
-	kmer_count = {}
-	replacements = {'A': 'CTG', 'C': 'ATG', 'G': 'CTA', 'T': 'CGA'}
-	for i in range(0, len(genome) - window + 1):
-		d = 0
-		slice = genome[i:i + window]
-		print(slice)
-		current_kmers = []
-		current_kmers.append(slice)
-		while d < mismatch:
-			for kmer in current_kmers:
-				for j in range(0, window - 1):
-					for char in replacements[slice[j]]:
-						temp_slice = kmer
-						temp_slice = list(temp_slice)
-						temp_slice[j] = char
-						temp_slice = "".join(temp_slice)
-						current_kmers.append(temp_slice)
-				d += 1
-				#print(current_kmers)
-				current_kmers = list(set(current_kmers))
-		for kmer in current_kmers:
-			if kmer not in kmer_count.keys():
-				kmer_count[kmer] = 1
-			else:
-				kmer_count[kmer] += 1
-	print(kmer_count)
-	print(max(kmer_count.iteritems(), key=operator.itemgetter(1))[0])
-	
-def test():
-	genome = 'ACGTTGCATGTCGCATGATGCATGAGAGCT'
-	window = 4
-	mismatch = 1
-	find_mismatch(genome, window, mismatch)
-	
-				
-		
+    kmer_count = {}
+    replacements = {'A': 'CTG', 'C': 'ATG', 'G': 'CTA', 'T': 'CGA'}
+    for i in range(0, len(genome) - window + 1):
+        d = 0
+        slice = genome[i:i + window]
+        print(slice)
+        current_kmers = []
+        next_kmers = []
+        current_kmers.append(slice)
+        while d < mismatch:
+            for kmer in current_kmers:
+                if mismatch == d:
+                    break
+                for j in range(0, window - 1):
+                    for char in replacements[slice[j]]:
+                        temp_slice = kmer
+                        temp_slice = list(temp_slice)
+                        temp_slice[j] = char
+                        temp_slice = "".join(temp_slice)
+                        current_kmers.append(temp_slice)
+                d += 1
+                print(current_kmers)
+                current_kmers = list(set(current_kmers))
+        for kmer in current_kmers:
+            if kmer not in kmer_count.keys():
+                kmer_count[kmer] = 1
+            else:
+                kmer_count[kmer] += 1
+    print(kmer_count)
+    print(max(kmer_count.iteritems(), key=operator.itemgetter(1))[0])
+    
+def main():
+    genome = 'ACGTTGCATGTCGCATGATGCATGAGAGCT'
+    window = 4
+    mismatch = 1
+    find_mismatch(genome, window, mismatch)
+
+if __name__ =="__main__":
+    main()
+
--- Week2/Protein Translation Problem/protein_translation.py
+++ Week2/Protein Translation Problem/protein_translation.py
@@ -1,9 +1,20 @@
+#Protein Translation Problem: Translate an RNA string into an amino acid string.
+#Input: An RNA string "Pattern"
+#Output: The translation of "Pattern" into an amino acid string "Peptide"
+
 def translate(pattern):
     translation_table = create_translation_table()
     peptide = ""
-    peptide += translation_table[i] for i in range(0, i + len(pattern) - 2)
+    i = 0
+    while i < len(pattern) - 2:
+        current_substring = pattern[i:i+3]
+        if translation_table[current_substring] == None:
+            break
+        peptide += translation_table[current_substring]
+        i += 3
     return peptide
- 
+
+#Creates a table for translating RNA chunks into a peptide string
 def create_translation_table():
     translation_table = {}
     with open('codon_table.txt') as lines:
@@ -12,13 +23,17 @@ def create_translation_table():
             codon = tokens[0]
             if len(tokens) > 1:
                 translation = tokens[1]
-                translation_table[tokens[0]] = tokens[1]
+                translation_table[codon] = translation
             else:
                 translation_table[tokens[0]] = None
     lines.close()   
-    return translation_table = {}
+    return translation_table
     
 def test():
     RNA_string = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
-    print(translate(RNA_string))
-    
+    expected_peptide = "MAMAPRTEINSTRING"
+    assert translate(RNA_string) == expected_peptide
+
+
+if __name__ =="__main__":
+    test()
